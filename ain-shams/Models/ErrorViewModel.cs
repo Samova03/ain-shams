3d77378bcc66289269b61ccb466eea62a67cced3
Fadwa Abisa <fadwaabisaHP@gmail.com>
2024-11-05 19:06:36 +0200

    new

---
+++ ain-shams/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ain_shams.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
